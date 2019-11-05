a873f76a6e8c9a7b7ce05eb3977ed0c568f05011
Becky Robran <rerobran@gmail.com>
2019-11-05 09:14:12 -0700

    Adds recipe file and test

---
+++ test/recipe_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+
+  def setup
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+  end
+
+  def test_it_exists
+    assert_instance_of Recipe, @mac_and_cheese
+  end
+end
